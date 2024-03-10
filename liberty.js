2cd5e318484aa781b2b36f45719cdeaa4a61e185
nelsonmakori <nelsonmakori6@gmail.com>
2024-03-10 17:44:24 -0600

    added more functionality

--- liberty.js
+++ liberty.js
@@ -7,6 +7,31 @@ liberty.addEventListener('submit',(data) => {
   console.log(frmData.get('email'))
   console.log(frmData.get('password'))
   console.log(frmData.get('repeatpassword'))
+
+  const login = document.getElementById('login')
+ let password = document.getElementById('password')
+ let passwordRepeate = document.getElementById('passwordRepeate')
+let displayMessage = document.getElementById('displayMessage')
+ let pin;
+
+  pin = passwordRepeate.value
+ pin = password.value;
+  pin = Number(pin);
+  
+  if(pin === 12345678){
+      displayMessage.textContent = `successfull logged in`
+    } else{
+      displayMessage.style.color ='red'
+      displayMessage.textContent ='wrong password !!'
+    }
+    
+  
+  
+  
+  //else{
+   // displayMessage.textContent = `wrong pin`
+  //}
+
 })
 
 const email = document.querySelector('#email')
@@ -15,7 +40,7 @@ const createAccount = document.querySelector('#createAcount')
 
 email.addEventListener('keyup',()=>{
   displayEmail.innerText = email.value
-  //createAccount.style.backgroundColor = 'green'
+  createAccount.style.backgroundColor = 'green'
 })
 
 createAccount.addEventListener('click',()=>{
@@ -23,9 +48,10 @@ createAccount.addEventListener('click',()=>{
   createAccount.style.fontSize = '10px';
 })
 
- const login = document.getElementById('login')
- let password = document.getElementById('password')
- let pin = 12345678
+ 
+  
+
+   
 
  
   
